f26dcb24f9e1a24342479f67f2d40dd56de0f5c8
Shivang Chaurasia <shiva17ng@gmail.com>
2026-03-02 22:06:34 +0530

    Time: 2 ms (82.46%), Space: 48 MB (89.67%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,15 +14,7 @@
  * }
  */
 class Solution {
-    static{
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
 
-    }
     public int height(TreeNode root){
         if(root==null){
             return 0;
