7b42ca62a6d2ff108009b19cdb73ec446214e86a
Shivang Chaurasia <shiva17ng@gmail.com>
2026-03-13 00:57:57 +0530

    Time: 1 ms (78%), Space: 42.5 MB (47.69%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int square(int n){
+        int num=0;
+        int temp = n;
+        while(temp>0){
+            int n1 = temp%10;
+            num = num + (n1*n1);
+            temp = temp/10;
+        }
+        return num;
+
+    }
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = square(n);
+        while(fast!=1 && fast!=slow){
+            slow = square(slow);
+            fast = square(square(fast));
+        }
+        return fast==1;
+    }
+}
