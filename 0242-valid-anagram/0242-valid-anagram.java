a2efb8bf5a7bcfc310738473d77e9227520f6d43
Shivang Chaurasia <shiva17ng@gmail.com>
2026-06-20 17:12:28 +0530

    Time: 3 ms (92.29%), Space: 46.2 MB (45.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,7 +6,31 @@ class Solution {
             Arrays.sort(s1);
             Arrays.sort(s2);
             return Arrays.equals(s1,s2);
-        }
-        return false;
+        }return false;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         int[] arr = new int[26];
+
+//         for(int i=0;i<s.length();i++){
+//             arr[s.charAt(i)-'a']++;
+//             arr[t.charAt(i)-'a']--;
+//         }
+
+
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]>0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
