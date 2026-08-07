6bb0e068908e7b699348c9a010843b5c11c82977
Shivang Chaurasia <shiva17ng@gmail.com>
2026-08-07 18:12:30 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.73%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,64 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int i=m-1;
+        int j=n-1;
+        int k=nums1.length-1;
+
+        while(i>=0 && j>=0){
+            if(nums1[i]<=nums2[j]){
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }else{
+                nums1[k]=nums1[i];
+                k--;
+                i--;
+            }
+        }
+        while(j>=0){
+            nums1[k--]=nums2[j--];
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         List<Integer> ls = new ArrayList<>();
+//         int i=0;
+//         int j=0;
+//         if(n==0){
+//             return;
+//         }
+//         while(i<m && j<n){
+//             if(nums1[i]<=nums2[j]){
+//                 ls.add(nums1[i]);
+//                 i++;
+//             }else{
+//                 ls.add(nums2[j]);
+//                 j++;
+//             }
+//         }
+//         while(i<m){
+//             ls.add(nums1[i++]);
+//         }
+//         while(j<n){
+//             ls.add(nums2[j++]);
+//         }
+
+
+//         for(int k=0;k<ls.size();k++){
+//             nums1[k]=ls.get(k);
+//         }
+
+//     }
+// }
