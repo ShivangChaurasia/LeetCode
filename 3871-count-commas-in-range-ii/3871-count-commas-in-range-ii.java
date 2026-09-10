4bb0220e5273791ddad07015f2e3c262cc32a8e0
Shivang Chaurasia <shiva17ng@gmail.com>
2026-09-10 17:18:52 +0530

    Time: 1 ms (99.24%), Space: 42.6 MB (53.61%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -2,20 +2,13 @@ class Solution {
     public long countCommas(long n) {
         if(n<1000){
             return 0;
-        }
-        int size = 0;
-        long temp = n;
-        while(temp>0){
-            size++;
-            temp/=10;
-        }
-        if(size>3 && size<7){
+        }else if(n<1000000){
             return n-999;
-        }else if(size>6 && size<10){
+        }else if(n<1000000000L){
             return ((n-999999)*2)+(999999-999);
-        }else if(size>9 && size<13){
+        }else if(n<1000000000000L){
             return ((n-999999999)*3)+((999999999-999999)*2)+(999999-999);
-        }else if(size>12 && size<16){
+        }else if(n<1000000000000000L){
             return ((n-999999999999L)*4)+((999999999999L-999999999L)*3)+((999999999L-999999L)*2)+(999999-999);
         }else{
              return ((n - 999999999999999L) * 5) + ((999999999999999L - 999999999999L) * 4) + ((999999999999L - 999999999L) * 3) + ((999999999L - 999999L) * 2) + (999999L - 999L);
